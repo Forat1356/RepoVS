ae6ec25507198d9e2ec3065b58967c34563ea72f
Thomas Zebar <thomaszebar@gmail.com>
2020-10-05 00:38:20 -0400

    sdf

---
+++ TomAppsWeb1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TomAppsWeb1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
